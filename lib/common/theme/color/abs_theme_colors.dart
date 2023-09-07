6fa2e740a03e3dc9da3c545af24f03a7788ccc21
Taeryong Lee <teo0821@gmail.com>
2023-09-07 15:12:09 +0900

    Step-Stock

--- lib/common/theme/color/abs_theme_colors.dart
+++ lib/common/theme/color/abs_theme_colors.dart
@@ -57,4 +57,8 @@ abstract class AbstractThemeColors {
   Color get lessImportantText => AppColors.grey;
 
   Color get importantText => AppColors.white;
+
+  Color? get lessImportant => const Color.fromARGB(255, 152, 30, 30);
+
+  Color? get plus => const Color.fromARGB(255, 222, 71, 85);
 }
--- lib/screen/main/tab/stock/f_stock.dart
+++ lib/screen/main/tab/stock/f_stock.dart
@@ -1,15 +1,97 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/common/widget/w_image_button.dart';
 import 'package:flutter/material.dart';
 
 class StockFragment extends StatefulWidget {
-  const StockFragment({super.key});
+  const StockFragment({Key? key}) : super(key: key);
 
   @override
   State<StockFragment> createState() => _StockFragmentState();
 }
 
-class _StockFragmentState extends State<StockFragment> {
+class _StockFragmentState extends State<StockFragment>
+    with SingleTickerProviderStateMixin {
+  late final _tabController = TabController(length: 2, vsync: this);
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: context.appColors.roundedLayoutBackground,
+          pinned: true,
+          actions: [
+            ImageButton(
+              imagePath: '$basePath/icon/stock_search.png',
+              onTap: () {},
+            ),
+            ImageButton(
+              imagePath: '$basePath/icon/stock_calendar.png',
+              onTap: () {
+                context.showSnackbar('캘린더');
+              },
+            ),
+            ImageButton(
+              imagePath: '$basePath/icon/stock_settings.png',
+              onTap: () {},
+            ),
+          ],
+        ),
+        SliverToBoxAdapter(
+          child: Column(
+            children: [
+              title,
+              tabBar,
+            ],
+          ),
+        ),
+      ],
+    );
   }
+
+  Widget get title => Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          '토스증권'.text.size(24).bold.make(),
+          width10,
+          'S&P 500'
+              .text
+              .size(13)
+              .bold
+              .color(context.appColors.lessImportant)
+              .make(),
+          width10,
+          3919.29
+              .toComma()
+              .text
+              .size(13)
+              .bold
+              .color(context.appColors.plus)
+              .make(),
+        ],
+      ).pOnly(left: 20);
+
+  Widget get tabBar => Column(
+        children: [
+          TabBar(
+            onTap: (index) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+            labelStyle:
+                const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            labelPadding: const EdgeInsets.symmetric(vertical: 20),
+            indicatorColor: Colors.white,
+            indicatorPadding: const EdgeInsets.symmetric(horizontal: 20),
+            controller: _tabController,
+            tabs: [
+              '내 주식'.text.make(),
+              '오늘의 발견'.text.make(),
+            ],
+          ),
+          const Line(),
+        ],
+      );
 }

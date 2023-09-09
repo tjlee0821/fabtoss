936560a17c120a19b46e15592e0f12a15941faaf
Taeryong Lee <teo0821@gmail.com>
2023-09-09 14:31:04 +0900

    stock_mixin

--- lib/screen/main/tab/stock/vo/vo_stock.dart
+++ lib/screen/main/tab/stock/vo/vo_stock.dart
@@ -1,4 +1,7 @@
+import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/screen/main/tab/stock/vo/vo_popular_stock.dart';
+import 'package:flutter/material.dart';
+import 'package:get/utils.dart';
 
 class Stock extends PopularStock {
   final String stockImagePath;
